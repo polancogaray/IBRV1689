09419524a8abca889eec8fbb4db33b022aed449b
Leodanny Wuilber Polanco Garay <polancogaray@gmail.com>
2025-03-15 18:52:09 -0400

    Adicionando funcion de login

--- src/pages/sesion/loguinSesion.vue
+++ src/pages/sesion/loguinSesion.vue
@@ -39,13 +39,29 @@
         label="Iniciar sesión"
         class="full-width rounded-buttom"
         @click="onSubmit" />
+      <!-- Botón para enviar el formulario -->
+      <q-btn
+        size="1.1rem"
+        no-caps
+        color="primary"
+        label="Registrar usuario"
+        class="full-width rounded-buttom"
+        @click="register" />
+      <q-btn
+        size="1.1rem"
+        no-caps
+        color="primary"
+        label="cerrar sesión"
+        class="full-width rounded-buttom"
+        @click="logout" />
     </q-card-section>
   </q-card>
 </template>
 
 <script setup>
 import { ref } from "vue";
-
+import { auth } from "src/boot/firebase";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "firebase/auth";
 
 
 // Estados reactivos
@@ -65,15 +81,46 @@ const validatePassword = (password) => {
 };
 
 // Función para manejar el envío del formulario
-const onSubmit = () => {
+const onSubmit = async () => {
   if (validateEmail(email.value) && validatePassword(password.value)) {
     console.log("Inicio de sesión exitoso");
-    
+    await login(email.value,password.value);
     // Aquí puedes agregar la lógica para enviar los datos al servidor
   } else {
     console.log("Por favor, complete los campos correctamente");
 
     
+  }
+};
+
+// Registrar un nuevo usuario
+const register = async () => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email.value,password.value);
+    console.log("Usuario registrado:", userCredential.user);
+    
+  } catch (error) {
+    console.error("Error al registrar:", error.message);
+  }
+};
+
+// Iniciar sesión
+const login = async (email, password) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    console.log("Usuario autenticado:", userCredential.user);
+  } catch (error) {
+    console.error("Error al iniciar sesión:", error.message);
+  }
+};
+
+// Cerrar sesión
+const logout = async () => {
+  try {
+    await signOut(auth);
+    console.log("Sesión cerrada");
+  } catch (error) {
+    console.error("Error al cerrar sesión:", error.message);
   }
 };
 </script>
